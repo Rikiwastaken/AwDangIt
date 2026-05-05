84a8fc1698850315f549b441b0928cc505b634b7
yyna <me@yyna.xyz>
2026-05-05 17:56:49 +0200

    modified vegetation shader

--- Assets/Scripts/TargetTracker.cs
+++ Assets/Scripts/TargetTracker.cs
@@ -1,8 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetTracker : MonoBehaviour
 {
+    private MaterialModificator _materialModificator;
+    
     public int targets = 0;
+    private bool _corrupted = false;
+
+    private void Start()
+    {
+        _materialModificator = gameObject.GetComponent<MaterialModificator>();
+    }
+
+    private void Update()
+    {
+        if (!_corrupted && targets > 0)
+        {
+            Debug.Log("corrupted");
+            StartCoroutine(_materialModificator.SetCorruptionToValue(1.0f));
+            _corrupted = true;
+        }
+        if (_corrupted && targets == 0)
+        {
+            Debug.Log("no longer");
+            StartCoroutine(_materialModificator.SetCorruptionToValue(0.0f));
+            _corrupted = false;
+        }
+    }
 }
