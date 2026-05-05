9e47ea7fb9a4b31c576a56963e68caef7a1fd63c
arnaudkrafft <arnaud.krafft@telecomnancy.net>
2026-05-05 14:06:04 +0200

    Removed Useless code

--- Assets/TimerScript.cs
+++ Assets/TimerScript.cs
@@ -9,12 +9,6 @@ public class TimerScript : MonoBehaviour
     private float timerVal;
     private bool timerisPlaying;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
